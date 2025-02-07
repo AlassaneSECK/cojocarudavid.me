d8864d9e5d9a294cd4cfcd9cf0ada1d99f7d6c95
AlassaneSECK <125903032+AlassaneSECK@users.noreply.github.com>
2025-02-07 11:47:37 +0100

    first commit

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,13 +1,13 @@
-export const tag = "@cojocaru-david";
-export const author = "David Cojocaru";
+export const tag = "@seck-alassane";
+export const author = "Alassane SECK";
 
 export const defaultMeta = {
-  title: "David Cojocaru - Home",
+  title: "Alassane SECK - Home",
   description:
     "Discover the portfolio and blog of David Cojocaru, a 16-year-old software developer and cybersecurity enthusiast from Romania. Explore his expertise in web development, programming languages, and more.",
-  keywords: ["david cojocaru", "junior software developer", "junior", "cybersecurity enthusiast", "web development", "programming", "portfolio", "blog", "romania"],
+  keywords: ["alassane seck", "junior software developer", "junior", "cybersecurity enthusiast", "web development", "programming", "portfolio", "blog", "romania"],
   ogImage: {
     src: "/banner.png",
-    alt: "David Cojocaru - Home",
+    alt: "Alassane SECK - Home",
   },
 };
